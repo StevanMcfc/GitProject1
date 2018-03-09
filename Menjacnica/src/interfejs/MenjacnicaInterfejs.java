64a38c05fc429d9d52d69cf35c53a75654ecc176
StevanMcfc <stevasteva3@gmail.com>
2018-03-09 14:29:11 +0100

    Merge ne radi, pokusaj ispravke.

--- Menjacnica/src/interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/interfejs/MenjacnicaInterfejs.java
@@ -2,11 +2,13 @@ package interfejs;
 
 import java.util.GregorianCalendar;
 
+import menjacnica.Kurs;
+
 public interface MenjacnicaInterfejs {
 
 	public void dodajValutu(String valuta, String naziv, double kurs, GregorianCalendar datum);
 	public void obrisiValutu(String valuta, String naziv, GregorianCalendar datum);
-	public double vratiKurs(String valuta, String naziv, GregorianCalendar datum);
+	public Kurs vratiKurs(String valuta, String naziv, GregorianCalendar datum);
 	
 	
 	
--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,11 +1,13 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 	@Override
 	public void dodajValutu(String valuta, String naziv, double kurs, GregorianCalendar datum) {
 
@@ -17,8 +19,19 @@ public class Menjacnica implements MenjacnicaInterfejs {
 	}
 
 	@Override
-	public double vratiKurs(String valuta, String naziv, GregorianCalendar datum) {
-		return 0;
+	public Kurs vratiKurs(String valuta, String vrsta, GregorianCalendar datum) {
+		Valuta val = new Valuta();
+		val.setNaziv(valuta);
+		Kurs k = new Kurs();
+		k.setDatum(datum);
+		k.setVrsta(vrsta);
+		int obj=0;
+		
+		if(!valute.contains(val)) obj = valute.indexOf(val); else
+		throw new RuntimeException("Data valuta ne postoji u bazi");
+		
+		if(valute.get(obj).getKursevi().contains(k)) return k;
+		else throw new RuntimeException("Ne postoji kurs za taj dan");
 	}
 
 }
