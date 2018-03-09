517c90d98de930ce2c82f4659f54088b14a73672
StevanMcfc <stevasteva3@gmail.com>
2018-03-09 13:46:28 +0100

    Napravljena klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,24 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajValutu(String valuta, double kurs, GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public void obrisiValutu(String valuta, double kurs, GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public double vratiKurs(String valuta, double kurs, GregorianCalendar datum) {
+		return 0;
+	}
+
+}
